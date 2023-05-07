83b4892ddcbed5c220ed4afd7dc60978d4993e13
Alex-Satin <airis6904@gmail.com>
2023-05-07 13:15:43 +0300

    rate ser

--- src/rates/rates.controller.ts
+++ src/rates/rates.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RatesService } from './rates.service';
 
@@ -7,6 +7,7 @@ import { RatesService } from './rates.service';
 export class RatesController {
   constructor(private readonly reatesService: RatesService) {}
 
+  @Get()
   getRate(@Query('buy') buy: string, @Query('sell') sell: string) {
     return this.reatesService.getRate(buy, sell);
   }
